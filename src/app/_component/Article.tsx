ad41bf8ff5c8c8540d77854fd916a054e8e71a1a
tktkaws <ttkaws15@gmail.com>
2025-03-26 11:37:22 +0900

    Fix: Articleを修正

--- src/app/_component/Article.tsx
+++ src/app/_component/Article.tsx
@@ -1,7 +1,7 @@
 import type { Blog } from "@/app/_libs/microcms";
 import "github-markdown-css";
 import { renderToc } from "@/app/_libs/render-toc";
-import TableOfContents from "@/app/component/TableOfContents";
+import TableOfContents from "@/app/_component/TableOfContents";
 
 type Props = {
   data: Blog;
@@ -9,6 +9,7 @@ type Props = {
 
 export default async function Article({ data }: Props) {
   const toc = renderToc(data.body);
+  
   return (
     <article className="grid lg:grid-cols-[76%_20%] lg:gap-x-[4%] grid-cols-1">
       <div className="lg:col-start-2 row-start-1 col-start-1">
@@ -16,7 +17,7 @@ export default async function Article({ data }: Props) {
       </div>
       <div className="col-start-1 mt-12">
         <div
-          className="markdown-body [&_h1]:scroll-mt-16 [&_h2]:scroll-mt-16 [&_h3]:scroll-mt-16 dark:text-white dark:bg-[#121212]"
+          className="p-4 lg:p-8 py-8 lg:py-16 rounded-md markdown-body [&_h1]:scroll-mt-16 [&_h2]:scroll-mt-16 [&_h3]:scroll-mt-16 dark:text-white dark:bg-[#292D30]"
           dangerouslySetInnerHTML={{
             __html: data.body,
           }}
