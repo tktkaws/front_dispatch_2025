e879b32ff6ef02b9c29f1cdf86e849bd3edcdd8b
tktkaws <ttkaws15@gmail.com>
2025-03-27 14:09:36 +0900

    Refactor: Update tag page layout and component imports to use horizontal versions and improve styling

--- src/app/tags/[tagId]/page.tsx
+++ src/app/tags/[tagId]/page.tsx
@@ -1,8 +1,10 @@
-import { getBlogsList, getAllTags } from "@/app/_libs/microcms";
-import CardList from "@/app/component/CardList";
+import { getBlogsList, getTagsWithCount, getAllTags } from "@/app/_libs/microcms";
+import CardList from "@/app/_component/CardList";
 import { BLOG_LIST_LIMIT } from "@/app/_constants";
-import TitleContainer from "@/app/component/TitleContainer";
-import TagList from "@/app/component/TagList";
+// import TitleContainer from "@/app/component/TitleContainer";
+// import TagList from "@/app/component/TagList";
+import TagListHorizontal from "@/app/_component/TagListVertical";
+import TitleContainerHorizontal from "@/app/_component/TitleContainerVertical";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -21,7 +23,7 @@ export default async function TagPage({ params }: Props) {
     filters: `tags[contains]${tagId}`,
   });
 
-  const tags = await getAllTags();
+  const tags = await getTagsWithCount();
 
   // タグに紐付くコンテンツがない場合は404
   if (data.contents.length === 0) {
@@ -32,17 +34,21 @@ export default async function TagPage({ params }: Props) {
     <main 
       id="main-content" 
       tabIndex={-1} 
-      className="pt-12 grid grid-cols-[4%_92%_4%] xl:grid-cols-[1fr_1200px_1fr]"
+      className="pt-12 pb-12 grid grid-cols-[4%_92%_4%] xl:grid-cols-[1fr_1200px_1fr] bg-[#F5F4F0] dark:bg-[#202024]"
     >
       <div className="col-[2]">
-        <TitleContainer />
-        <TagList tags={tags.contents} />
-        <CardList 
-          data={data.contents}
-          totalCount={data.totalCount}
-          currentPage={1}
-          perPage={BLOG_LIST_LIMIT}
-        />
+        <div className="grid grid-cols-1 lg:grid-cols-[46%_46%] gap-x-[8%] p-[4%] md:p-8">
+        <TitleContainerHorizontal />
+        <TagListHorizontal tags={tags.contents} />
+        </div>
+        <div className="bg-white dark:bg-[#292D30] rounded-md p-[4%] md:p-4 lg:p-8 mt-8">
+          <CardList 
+            data={data.contents} 
+            totalCount={data.totalCount}
+            currentPage={1}
+            perPage={BLOG_LIST_LIMIT}
+          />
+        </div>
       </div>
     </main>
   );
